b1896ee3f44debd003c498a5e22f9dda5e22e049
Emily Potter <emily_p123@hotmail.co.uk>
2020-04-30 02:48:28 +0100

    Lat and long user input for place search working

--- app/src/main/java/com/example/mswproject/MainActivity.kt
+++ app/src/main/java/com/example/mswproject/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.mswproject
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
@@ -8,20 +12,40 @@ import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONException
 import org.json.JSONObject
+import java.lang.Exception
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("fwaf", "fdef")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchButton = findViewById<Button>(R.id.search_button)
+
+
+        searchButton.setOnClickListener(this)
+    }
+
+    private fun onButtonPress(latitude: Float?, longitude: Float?) {
+
+    }
+
+    override fun onClick(v: View?) {
         val textView = findViewById<TextView>(R.id.text)
+        val latitudeString = findViewById<EditText>(R.id.latitude_text).text.toString()
+        val latitude = latitudeString.toDouble()
+        val longitudeString = findViewById<EditText>(R.id.longitude_text).text.toString()
+        val longitude = longitudeString.toDouble()
 
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyCyRBE0IR0jmxLf61_9jmlH3Fphb5p4LpI"
+        //val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=cafe&keyword=surf&key=AIzaSyCyRBE0IR0jmxLf61_9jmlH3Fphb5p4LpI"
+        val url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=lodge&keyword=surf&key=AIzaSyCyRBE0IR0jmxLf61_9jmlH3Fphb5p4LpI"
+        Log.d("click", url)
 
         // Request a string response from the provided URL.
         val jsonObjectRequest = JsonObjectRequest(
@@ -34,10 +58,25 @@ class MainActivity : AppCompatActivity() {
                 while (x < jsonArray.length()) {
                     val jsonObject = jsonArray.getJSONObject(x)
 
+                    var openString = ""
+                    if (jsonObject.has("opening_hours")) {
+                        val openingHours = jsonObject.getJSONObject("opening_hours")
+                        val open = openingHours.getBoolean("open_now")
+                        if (open) {
+                            openString = "Open now"
+                        } else if (!open) {
+                            openString = "Closed"
+                        }
+                    } else {
+                        openString = "Opening times unavailable"
+                    }
                     list.add(
                         Place(
-                        jsonObject.getString("name")
-                    ))
+                            jsonObject.getString("name"),
+                            jsonObject.getInt("rating"),
+                            openString
+                        )
+                    )
                     x++
                 }
 
